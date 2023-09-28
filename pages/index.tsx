30300a41d53d471a60b245d45578717e0b612c41
taoky <me@taoky.moe>
2023-09-28 14:34:43 +0800

    router.push version

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 /** Add your relevant code here for the issue to reproduce */
-import Link from 'next/link';
+// import Link from 'next/link';
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter()
+
   return <div>
-    <Link href="a/">a/</Link>
+    {/* <Link href="a/">a/</Link>
     <br/>
-    <Link href="b.git/">b.git/</Link>
+    <Link href="b.git/">b.git/</Link> */}
+    <button type="button" onClick={() => router.push('a/')}>a/</button>
+    <br/>
+    <button type="button" onClick={() => router.push('b.git/')}>b.git/</button>
   </div>
 }
